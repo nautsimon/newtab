c5abdc22d3d7515c6f650c8912e0b9fb97225ab8
Simon mahns <ak.simonm@gmail.com>
2018-05-11 18:59:46 -0400

    broke

--- homepage.js
+++ homepage.js
@@ -1,51 +1,47 @@
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
-  todo: [],
-  completed: []
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{ //stores the stuff in local storage
+  todo: [], //initializes the todo list array
+  completed: [] //creates the completed arry
 };
-
+//data.todo is simply the array being called from that part of the computer
 // Remove and complete icons in SVG format
-renderTodoList();
+renderTodoList(); //renders the to do list
 
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
-document.getElementById('addtoday').addEventListener('click', function() {
-  var value = document.getElementById('item').value;
-  if (value) {
-    addItem(value);
-  }
-});
-
-document.getElementById('addlater').addEventListener('click', function() {
-  var value = document.getElementById('item').value;
-  if (value) {
-    addItem(value);
-  }
-});
-
-document.getElementById('item').addEventListener('keydown', function (e) {
-  var value = this.value;
-  if (e.code === 'Enter' && value) {
-    addItem(value);
-  }
-});
-
-function addItem (value) {
-  addItemToDOM(value);
+document.getElementById('addtoday').addEventListener('click', function() { //listens to button click
+  var value = document.getElementById('item').value; //sets a new variable to the value in the input which is identified by the id item
   document.getElementById('item').value = '';
+  if (value) { //checks to see if anything is inside the input
+    addItem(value);
+     //calls method addItem and inputs the value
+  }
+});
 
-  data.todo.push(value);
-  dataObjectUpdated();
+document.getElementById('addlater').addEventListener('click', function() { //same as a above but listens to the to do later button
+  var value = document.getElementById('item').value;
+  document.getElementById('item').value = '';
+  if (value) {
+    addItem(value);
+  }
+});
+
+function addItem (value) { //the additem method
+  addItemToDOM(value); //invokes the additem to dom meethod with value as the input
+
+   //resets the input to a blank slate
+  data.todo.push(value); 
+  dataObjectUpdated(); 
 }
 
 function renderTodoList() {
-  if (!data.todo.length && !data.completed.length) return;
+  if (!data.todo.length && !data.completed.length) return; //if the lengths of both lists are zero then return
 
-  for (var i = 0; i < data.todo.length; i++) {
-    var value = data.todo[i];
-    addItemToDOM(value);
+  for (var i = 0; i < data.todo.length; i++) { //for loop that goes through the entire todo array
+    var value = data.todo[i];  //makes a new variable that is at the value at the current index
+    addItemToDOM(value);  //adds the value to the the front page
   }
 
-  for (var j = 0; j < data.completed.length; j++) {
+  for (var j = 0; j < data.completed.length; j++) { //does the same as the top except is for the completed array
     var value = data.completed[j];
     addItemToDOM(value, true);
   }
@@ -94,32 +90,33 @@ function completeItem() {
 }
 
 // Adds a new item to the todo list
-function addItemToDOM(text, completed) {
+function addItemToDOM(text, completed) { //completed is boolean to see if it is completed
   var list = (completed) ? document.getElementById('completed'):document.getElementById('todo');
 
-  var item = document.createElement('li');
+  var item = document.createElement('li'); //make new list element
   item.innerText = text;
 
-  var buttons = document.createElement('div');
-  buttons.classList.add('buttons');
 
-  var remove = document.createElement('button');
-  remove.classList.add('remove');
-  remove.innerHTML = removeSVG;
+  var buttons = document.createElement('div'); //creates new button for removal
+  buttons.classList.add('buttons');  //adds css
+
+  var remove = document.createElement('button'); //new removal button
+  remove.classList.add('remove'); //adds css
+
+
 
   // Add click event for removing the item
-  remove.addEventListener('click', removeItem);
+  remove.addEventListener('click', removeItem); 
 
   var complete = document.createElement('button');
-  complete.classList.add('complete');
-  complete.innerHTML = completeSVG;
+  complete.classList.add('complete'); //adds css
 
-  // Add click event for completing the item
-  complete.addEventListener('click', completeItem);
+  document.getElementById('item').value = '';
+  complete.addEventListener('click', completeItem); // Add click event for completing the item
 
-  buttons.appendChild(remove);
-  buttons.appendChild(complete);
-  item.appendChild(buttons);
+  buttons.appendChild(remove); //puts the removal button into the buttons div
+  buttons.appendChild(complete); //puts the complete button into the buttons div
+  item.appendChild(buttons); //puts the buttons div within the li 
 
   list.insertBefore(item, list.childNodes[0]);
 }
