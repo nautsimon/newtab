c04d7d971b4bb1017ceaef173e06b06229aa8c47
Simon mahns <ak.simonm@gmail.com>
2018-08-19 18:11:44 -0400

    particle js config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Particles from "react-particles-js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Particles
+          params={{
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#bbbbb",
+                  blur: 1
+                }
+              },
+              canvas: {
+                width: "100%",
+                height: "100%"
+              }
+            }
+          }}
+          // style={{
+          //   height: "100px"
+          // }}
+        />
       </div>
     );
   }
 }
-
 export default App;
